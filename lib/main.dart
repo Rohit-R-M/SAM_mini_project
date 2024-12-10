48f9174916fe2a73836b26b8e44be22f72753f14
sagar <sagarsguggari2004@gmail.com>
2024-12-10 19:45:26 +0530

    10/12/2024-integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sam_pro/splashscreen.dart';
-import 'Teacher/UploadResult/testing.dart';
 
 
 Future<void> main() async{
